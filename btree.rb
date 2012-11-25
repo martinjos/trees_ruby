6a727999d0d855abce1d14a65581caa061da7fb7
Martincode <anonymous@example.com>
2012-11-25 18:52:11 +0000

    Add remove method.

--- btree.rb
+++ btree.rb
@@ -9,6 +9,10 @@ class NullNode
 	[ true, Node.new(@fsize, @hfsize, @compar, [self, value, self]) ]
     end
 
+    def remove(value)
+	self
+    end
+
     def dump(stack)
 	# do nothing
     end
@@ -65,6 +69,67 @@ class Node < Array
 	end
     end
 
+    def remove(value=nil)
+	idx = self.index(value)
+	if idx % 2 == 0
+	    self[idx].remove value
+	    if idx == 0
+		midx = 0
+	    else
+		midx = idx - 2
+	    end
+	elsif self[0].is_a? NullNode
+	    self.slice! idx, 2
+	else
+	    self[idx] = self[idx + 1].remove_first
+	    midx = idx - 1
+	end
+
+	# there are two cases: removed from here, and "removed" from null node
+	if !self[0].is_a? NullNode
+	    self.insert midx, *self.merge(self.slice!(midx, 3))
+	end
+    end
+
+    def remove_first
+	if self[0].is_a? NullNode
+	    first = self[1]
+	    self.slice!(0, 2)
+	else
+	    first = self[0].remove_first
+	    self.insert 0, *self.merge(self.slice!(0, 3))
+	end
+	first
+    end
+
+    def merge((a, b, c))
+	# a or c can be at most 2 (i.e 1 full segment) less than #{hfsize}
+	# if either is larger than hfsize, it will be greater in units of 2
+	# (i.e. whole segments)
+
+	#puts "Merging with a.size=#{a.size}, c.size=#{c.size}, @hfsize=#{@hfsize}"
+	#puts "a:"
+	#a.dump
+	#puts "b: #{b}"
+	#puts "c:"
+	#c.dump
+
+	if a.size < @hfsize && c.size > @hfsize
+	    a << b
+	    a << c[0]
+	    b = c[1]
+	    c.slice! 0, 2
+	elsif a.size > @hfsize && c.size < @hfsize
+	    c.unshift b
+	    c.unshift a[-1]
+	    b = a[-2]
+	    a.slice! -2, 2
+	elsif (a.size <=> @hfsize) + (c.size <=> @hfsize) < 0
+	    return [Node.new(@fsize, @hfsize, @compar, a + [b] + c)]
+	end
+	[a, b, c]
+    end
+
     def indent(stack)
 	stack.each_with_index{|item, idx|
 	    print idx == stack.size - 1 ? " \\_ "
@@ -106,11 +171,31 @@ class BTree
 	(did_split, @head) = @head.add(value)
     end
 
+    def remove(value)
+	@head.remove(value)
+	# empty. not strictly necessary, but restores initial state
+	if @head.size == 1
+	    @head = @head[0]
+	end
+    end
+
     def dump
 	@head.dump
     end
 end
 
-def bhash(size = 3, compar = ->(a,b){a <=> b})
+def bmap(size = 3, compar = ->(a,b){a <=> b})
     BTree.new(size, ->(a,b){ compar.call(a[0], b[0]) })
+end
+
+# add #{num} random integers in [ 0, #{top} ) to a ( #{size-1}, #{size} )
+# B-tree and dump in "ps f" format
+#
+def testadd(size, num, top)
+    t = BTree.new(size)
+    (0...num).each{
+	t.add rand(0...top)
+    }
+    t.dump
+    t
 end
